a2210048c5106c13acb97685c7409f6f7fb2d7d9
j21O2 <166688413+j21O2@users.noreply.github.com>
2024-07-04 06:12:41 +0000

    chapter14

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 //const pg = require('pg');
@@ -44,6 +44,54 @@ res.render('index', {
 title: 'ToDo App',
 });
 });
+});
+
+module.exports = router;
+
+
+*/
+const express = require('express');
+const router = express.Router();
+const pg = require('pg');
+
+const connection = new pg.Pool({
+  host: 'localhost',
+  user: 'root',
+  password: 'postgres',
+  database: 'todo_app',
+  port: 5432,
+});
+
+router.get('/', function (req, res, next) {
+  connection.query(
+    `select * from tasks;`,
+    (error, results) => {
+      console.log(error);
+      console.log(results.rows);
+      res.render('index', {
+        title: 'ToDo App',
+        todos: results.rows,
+      });
+    }
+  );
+});
+
+router.post('/', function (req, res, next) {
+  connection.connect((err) => {
+    if (err) {
+      console.log('error connecting: ' + err.stack);
+      return
+    }
+    console.log('success');
+  });
+  const todo = req.body.add;
+  connection.query(
+    `insert into tasks (user_id, content) values (1, '${todo}');`,
+    (error, results) => {
+      console.log(error);
+      res.redirect('/');
+    }
+  );
 });
 
 module.exports = router;
